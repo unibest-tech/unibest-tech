106952618c2a713da4761488a489b7a712e4cb62
feige996 <1020102647@qq.com>
2025-08-25 00:57:17 +0800

    feat: 更新博客链接和添加弹窗组件，删除旧日志文件

--- docs/.vitepress/theme/components/HomeFeaturesAfter.vue
+++ docs/.vitepress/theme/components/HomeFeaturesAfter.vue
@@ -2,10 +2,12 @@
   <ShowCase />
   <ProjectCreateCountChart />
   <!-- <HomeDialog /> -->
+  <HomeDialog2 />
 </template>
 
 <script setup>
 import ShowCase from './showcase/Showcase.vue';
 import ProjectCreateCountChart from './project-create-count-chart/ProjectCreateCountChart.vue';
 // import HomeDialog from './HomeDialog.vue';
+import HomeDialog2 from './HomeDialog2.vue';
 </script>
